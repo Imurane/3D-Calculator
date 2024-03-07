1e7ccfebeb4cc54d892514d2172f9cfbb12b109f
Imurane <160079036+Imurane@users.noreply.github.com>
2024-03-07 23:07:30 +0100

    Update main.js

--- 3D-Calc/main.js
+++ 3D-Calc/main.js
@@ -18,7 +18,7 @@ function result()
 {
     btnre.play();
     try{
-        if(display.value == '0'){display.value = 'chfanja';}
+        if(eval(calc()) == 0 ){display.value = 'chfanja';}
         else{display.value = eval(calc());}
     }
     catch(error){
