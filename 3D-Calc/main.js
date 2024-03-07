57352ff07725a4d1cffdc2e31b9c1b03fbf0b7a1
Imurane <160079036+Imurane@users.noreply.github.com>
2024-03-07 23:42:40 +0100

    Update main.js

--- 3D-Calc/main.js
+++ 3D-Calc/main.js
@@ -4,7 +4,7 @@ var btnr = new Audio("btn-reset.mp3");
 var btnre = new Audio("btn-result.mp3");
 function calc (val="")
 {
-    if(display.value == 'Error' || display.value == 'NaN' || display.value == 'Infinity' || display.value == 'undefined' || display.value == 'chfnja'){display.value = '';}
+    if(display.value == 'Error' || display.value == 'NaN' || display.value == 'Infinity' || display.value == 'undefined' || display.value == 'RIP' || display.value == 'chfnja'){display.value = '';}
     display.value += val;
     btnc.play();
     return display.value;
@@ -18,7 +18,8 @@ function result()
 {
     btnre.play();
     try{
-        if(eval(calc()) == 0 ){display.value = 'chfnja';}
+        if(display.value == '1-1'){display.value = 'RIP';}
+        else if(eval(calc()) == 0 ){display.value = 'chfnja';}
         else{display.value = eval(calc());}
     }
     catch(error){
